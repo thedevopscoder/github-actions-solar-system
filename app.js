c2c8de0f4566e4404d9c561ad6d15f3b99620be5
thedevopscoder <100053627+thedevopscoder@users.noreply.github.com>
2024-01-28 23:43:29 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,99 +1,79 @@
 const path = require('path');
 const express = require('express');
 const OS = require('os');
-const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const cors = require('cors');
+
 const app = express();
-const cors = require('cors')
 
-
-// app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/')));
-app.use(cors())
-
-// mongoose.connect(process.env.MONGO_URI, {
-//     user: process.env.MONGO_USERNAME,
-//     pass: process.env.MONGO_PASSWORD,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }, function(err) {
-//     if (err) {
-//         console.log("error!! " + err)
-//     } else {
-//       //  console.log("MongoDB Connection Successful")
-//     }
-// })
+app.use(cors());
 
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0.va9qn.mongodb.net/solar`
+    `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@cluster0.va9qn.mongodb.net/solar`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
   )
-  .then(() => {
-    console.log("Connected to Database");
-  })
-  .catch((err) => {
-    console.log(err);
+  .then(() => console.log("Connected to Database"))
+  .catch((err) => console.log(err));
+
+const dataSchema = new mongoose.Schema({
+  name: String,
+  id: Number,
+  description: String,
+  image: String,
+  velocity: String,
+  distance: String
+});
+
+const planetModel = mongoose.model('planets', dataSchema);
+
+app.post('/planet', async (req, res) => {
+  try {
+    const planetData = await planetModel.findOne({ id: req.body.id });
+    if (!planetData) {
+      throw new Error("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9");
+    }
+    res.send(planetData);
+  } catch (err) {
+    res.status(500).send("Error in Planet Data: " + err.message);
+  }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
+
+app.get('/os', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send({
+    "os": OS.hostname(),
+    "env": process.env.NODE_ENV
   });
-
-var Schema = mongoose.Schema;
-
-var dataSchema = new Schema({
-    name: String,
-    id: Number,
-    description: String,
-    image: String,
-    velocity: String,
-    distance: String
-});
-var planetModel = mongoose.model('planets', dataSchema);
-
-
-
-app.post('/planet',   function(req, res) {
-   // console.log("Received Planet ID " + req.body.id)
-    planetModel.findOne({
-        id: req.body.id
-    }, function(err, planetData) {
-        if (err) {
-            alert("Ooops, We only have 9 planets and a sun. Select a number from 0 - 9")
-            res.send("Error in Planet Data")
-        } else {
-            res.send(planetData);
-        }
-    })
-})
-
-app.get('/',   async (req, res) => {
-    res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
+app.get('/live', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send({
+    "status": "live"
+  });
+});
 
-app.get('/os',   function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "os": OS.hostname(),
-        "env": process.env.NODE_ENV
-    });
-})
+app.get('/ready', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send({
+    "status": "ready"
+  });
+});
 
-app.get('/live',   function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "status": "live"
-    });
-})
+const PORT = process.env.PORT || 3000;
 
-app.get('/ready',   function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
-        "status": "ready"
-    });
-})
-
-app.listen(3000, () => {
-    console.log("Server successfully running on port - " +3000);
-})
-
+app.listen(PORT, () => {
+  console.log(`Server successfully running on port - ${PORT}`);
+});
 
 module.exports = app;
